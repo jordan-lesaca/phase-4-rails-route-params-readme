a9c5cc2c969aca81d3cc785f052bff96b0940b06
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-12-07 21:24:24 -0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+  get '/cheeses/:id', to: 'cheeses#show' #1 add a GET route
+  #1 the /:id tells the routing system that this route can perceive a parameter and that the parameter will be passed to the controller's show action. 
+
+  
 end
